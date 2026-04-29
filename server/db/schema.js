948aa9259d02f2250d13c4e0bc66673e3386dfc0
udsey <yuuhik@yandex.ru>
2026-04-29 13:24:47 +0700

    feat: implement queue system for batching DB writes and update casts schema to use UUID

--- server/db/schema.js
+++ server/db/schema.js
@@ -1,13 +1,11 @@
 import { sql } from 'drizzle-orm';
-import { pgTable, serial, text, timestamp, real, integer, index } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text, timestamp, real, integer, index, uuid } from 'drizzle-orm/pg-core';
 
 
 export const casts = pgTable('casts', {
-  id: serial('id').primaryKey(),
+  id: uuid('id').primaryKey(),
   text: text('text').notNull(),
-  createdAt: timestamp('created_at').notNull().defaultNow(),
-  
-  // Allow NULL initially
+  createdAt: timestamp('created_at'),
   x: real('x'),
   y: real('y'),
   fontSize: real('font_size'),
